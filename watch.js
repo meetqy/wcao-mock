970f56dfb61050a37cdcf736f19e907316b96c87
xiaoqiyang <cyou_yang@163.com>
2022-05-21 17:03:15 +0800

    database支持mockjs

--- watch.js
+++ watch.js
@@ -1,16 +1,18 @@
 const chokidar = require("chokidar");
 const fs = require("fs");
+const Mock = require("mockjs");
 
 let db = {};
 
 const watcher = chokidar.watch("./database/*.json");
 
 watcher.on("all", (event, path) => {
-  const cur_db = JSON.parse(fs.readFileSync("./" + path, "utf8"));
+  const cur_db = Mock.mock(JSON.parse(fs.readFileSync("./" + path, "utf8")));
   const prefix = path.replace("database/", "").replace(".json", "");
 
   // filename
   db[prefix] = cur_db;
+  console.log(db[prefix]);
 
   for (let key in cur_db) {
     db[`${prefix}@${key}`] = cur_db[key];
